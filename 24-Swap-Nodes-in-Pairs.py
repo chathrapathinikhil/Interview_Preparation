6cb868d72f0a6b7d24cf99a9c865dd57dc0dced7
Chathrapathi Nikhil Kandagatla <42908027+chathrapathinikhil@users.noreply.github.com>
2024-09-08 14:18:32 -0700

    [24] [Time Beats: 87.14%] [Memory Beats: 19.83%] - LeetPush

--- 24-Swap-Nodes-in-Pairs.py
+++ 24-Swap-Nodes-in-Pairs.py
@@ -9,19 +9,23 @@ class Solution:
         dummy = ListNode(0)
         temp, temp1 = head, dummy
         count = 1
-        while temp:
-            print(stack)
-            if count%2 == 1:
-                stack.append(temp.val)
-                temp = temp.next
+        if head==None:
+            return None
+        elif head.next == None:
+            return head
+        else:
+            while temp:
+                if count%2 == 1:
+                    stack.append(temp.val)
+                    temp = temp.next
+                    count+=1
+                    continue
+                temp1.next = ListNode(temp.val)
+                temp1 = temp1.next
+                temp1.next = ListNode(stack.pop())
                 count+=1
-                continue
-            temp1.next = ListNode(temp.val)
-            temp1 = temp1.next
-            temp1.next = ListNode(stack.pop())
-            count+=1
-            temp1 = temp1.next
-            temp = temp.next
-        if stack:
-            temp1.next = ListNode(stack.pop())
-        return dummy.next
+                temp1 = temp1.next
+                temp = temp.next
+            if stack:
+                temp1.next = ListNode(stack.pop())
+            return dummy.next
