6d4ee51c6836559c2995ab2aaa0d6de590f324f3
Chathrapathi Nikhil Kandagatla <42908027+chathrapathinikhil@users.noreply.github.com>
2024-10-17 15:07:01 -0700

    [577] [Time Beats: 46.11%] - LeetPush

--- 577-Employee-Bonus.sql
+++ 577-Employee-Bonus.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select name, bonus from employee as e left join bonus as b on e.empid = b.empid where (bonus < 1000 or bonus is Null) order by b.bonus
+select name, bonus from employee e left join bonus b on e.empid = b.empid where (bonus < 1000 or bonus is Null)
